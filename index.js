b97c8299162a14c27f18a4ae3e576b16d689b288
yyRuth <106503937+yyRuth@users.noreply.github.com>
2024-09-08 00:19:11 -0300

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Playing VALORANT"];
+const statusMessages = ["Listening to Spotify","Playing Roblox","Playing Minecraft","Eating Miojo","Checking Damazio App"],
 
 
 let currentIndex = 0;
